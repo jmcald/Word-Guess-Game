382b6d7d08ebc563cb8907e95bde6e7085673d72
John Caldwell <jmcald0@gmail.com>
2019-01-29 12:16:51 -0600

    looks ok

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,162 @@
-var country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Botswana","Brazil","Brunei","Bulgaria","Burundi","Cambodia","Cameroon","Canada","Chad","Chile","China","Colombia","Congo","Croatia","Cuba","Cyprus","Denmark","Djibouti","Dominica","Ecuador","Egypt","Estonia","Ethiopia","Fiji","Finland","France","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guyana","Haiti","Honduras","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Paraguay","Peru","Philippines","Poland","Portugal","Qatar","Reunion","Romania","Russia","Rwanda","Samoa","Senegal","Serbia","Seychelles","Singapore","Slovakia","Slovenia","Spain","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Togo","Tonga","Tunisia","Turkey","Turkmenistan","Uganda","Ukraine","Uruguay","Uzbekistan","Venezuela","Vietnam","Yemen","Zambia","Zimbabwe"];
-var flag_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Botswana","Brazil","Brunei","Bulgaria","Burundi","Cambodia","Cameroon","Canada","Chad","Chile","China","Colombia","Congo","Croatia","Cuba","Cyprus","Denmark","Djibouti","Dominica","Ecuador","Egypt","Estonia","Ethiopia","Fiji","Finland","France","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guyana","Haiti","Honduras","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Paraguay","Peru","Philippines","Poland","Portugal","Qatar","Reunion","Romania","Russia","Rwanda","Samoa","Senegal","Serbia","Seychelles","Singapore","Slovakia","Slovenia","Spain","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Togo","Tonga","Tunisia","Turkey","Turkmenistan","Uganda","Ukraine","Uruguay","Uzbekistan","Venezuela","Vietnam","Yemen","Zambia","Zimbabwe"];
-var map_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Botswana","Brazil","Brunei","Bulgaria","Burundi","Cambodia","Cameroon","Canada","Chad","Chile","China","Colombia","Congo","Croatia","Cuba","Cyprus","Denmark","Djibouti","Dominica","Ecuador","Egypt","Estonia","Ethiopia","Fiji","Finland","France","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guyana","Haiti","Honduras","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Paraguay","Peru","Philippines","Poland","Portugal","Qatar","Reunion","Romania","Russia","Rwanda","Samoa","Senegal","Serbia","Seychelles","Singapore","Slovakia","Slovenia","Spain","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Togo","Tonga","Tunisia","Turkey","Turkmenistan","Uganda","Ukraine","Uruguay","Uzbekistan","Venezuela","Vietnam","Yemen","Zambia","Zimbabwe"];
+var countryGuessGame ={
+    listObject: {
+        Afghanistan:{
+            flag: "afg-flg.png"
+        },
+        Austria: {
+            flag: "aus-flg.png"
+        },
+        Belgium: {
+            flag: "bel-flg.png"
+        },
+        Canada: {
+            flag: "can-flg.png"
+        },
+        France: {
+            flag: "fra-flg.png"
+        },
+        Germany: {
+            flag: "ger-flg.png"
+        },
+        Haiti: {
+            flag: "hai-flg.png"
+        },
+        India: {
+            flag: "ind-flg.png"
+        },
+        Japan: {
+            flag: "jap-flg.png"
+        },
+        Kenya: {
+            flag: "ken-flg.png"
+        },
+        Lebanon: {
+            flag: "leb-flg.jpg"
+        },
+        Netherlands: {
+            flag: "net-flg.png"
+        },
+        Poland: {
+            flag: "pol-flg.png"
+        },
+        Russia: {
+            flag: "rus-flg.png"
+        },
+        Spain: {
+            flag: "spa-flg.png"
+        },
+        Sweden: {
+            flag: "swe-flg.png"
+        },
+        Turkey: {
+            flag: "tur-flg.png"
+        },
+        Zimbabwe: {
+            flag: "zim-flg.png"
+        }
+    },
+
+    currentWord: null,
+    currentWordLetters: [],
+    correctLetters: [],
+    guessedLetters: [],
+    guessesLeft: 0,
+    totalGuesses: 0,
+    letterGuessed: null,
+    wins: 0,
+
+    initializeGame: function() {
+        var objKeys = Object.keys(this.listObject);
+        this.currentWord = objKeys[Math.floor(Math.random()* objKeys.length)];
+
+        this.currentWordLetters = this.currentWord.split("")
+        this.rebuildWordView();
+        this.processUpdateTotalGuesses();
+    },
+
+    updatePage: function(letter) {
+        if(this.guessesLeft === 0) {
+            this.restartGame();
+        }
+        else {
+            this.updateGuesses(letter);
+            this.updateCorrectLetters(letter);
+            this.rebuildWordView();
+            if (this.updateWins() === true) {
+                this.restartGame();
+            }
+        }
+    },
+    updateGuesses: function(letter) {
+        if((this.guessedLetters.indexOf(letter) === -1) &&(this.currentWordLetters.indexOf(letter) === -1)) {
+            this.guessedLetters.push(letter);
+            this.guessesLeft--;
+            document.querySelector("#guess-lim").innerHTML = this.guessesLeft;
+            document.querySelector("#wrong-guess").innerHTML = this.guessedLetters.join(", ");
+        }
+    },
+    processUpdateTotalGuesses: function() {
+        this.totalGuesses = this.currentWordLetters.length + 5;
+        this.guessesLeft = this.totalGuesses;
+        document.querySelector("#guess-rem").innerHTML = this.guessesLeft;
+    },
+    updateCorrectLetters: function(letter) {
+        for (i=0; i<this.currentWordLetters.length; i++) {
+            if((letter === this.currentWordLetters[i]) && (this.correctLetters.indexOf(letter) === -1)) {
+                this.matchedLetters.push(letter);
+            }
+        }
+    },
+    rebuildWordView: function() {
+        var wordView = "";
+        for(i=0; i<this.currentWordLetters.length; i++) {
+            if (this.correctLetters.indexOf(this.currentWordLetters[i]) !== -1) {
+                wordView += this.currentWordLetters[i];
+            }
+            else {
+                wordView+= "&nbsp;_&nbsp;";
+            }
+        }
+        document.querySelector("#myst-country").innerHTML = wordView;
+    },
+    restartGame: function() {
+        document.querySelector("#wrong-guess").innerHTML = "";
+        this.currentWord = null;
+        this.currentWordLetters = [];
+        this.correctLetters = [];
+        this.guessedLetters = [];
+        this.guessesLeft = 0;
+        this.totalGuesses = 0;
+        this.letterGuessed = null;
+        this.setupGame();
+        this.rebuildWordView();
+    },
+    updateWins: function() {
+        var win;
+        if( this.correctLetters.length === 0) {
+            win = false;
+        }
+        else {
+            win = true;
+        }
+        for (i=0; i<this.currentWordLetters.length; i++) {
+            if (this.correctLetters.indexOf(this.currentWordLetters[i]) === -1) {
+                win = false;
+            }
+        }
+        if (win) {
+            this.wins = this.wins +1;
+            document.querySelector("winloss-int").innerHTML = this.wins;
+            document.querySelector("#flag").innerHTML = "<img class='flag-image' src='../images/" + this.listObject[this.currentWord].flag + ">";
+            return true;
+        }
+        return false;
+    }
+};
+
+countryGuessGame.setupGame();
+
+document.onkeyup = function(event) {
+    countryGuessGame.letterGuessed = String.fromCharCode(event.which).toLowerCase();
+    countryGuessGame.updatePage(countryGuessGame.letterGuessed);
+};
